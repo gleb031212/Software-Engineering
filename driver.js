9db91854c06d2a20bfcfc19d8d61a2c8191b0ad8
Benjamaxo <76113399+Benjamaxo@users.noreply.github.com>
2024-04-04 15:37:11 +0100

    added initial driver file

---
+++ driver.js
@@ -0,0 +1,18 @@
+class Driver {
+    
+    constructor(username, email, password) {
+        let balance = 0;
+    }
+    parkingRequest(destination, arriveTime, endTime) {
+        // Returns the assigned parking space
+    }
+    makePayment(amount) {
+        balance = balance + amount;
+    }
+    parkingRequest(space, arriveTime, endTime) {
+        // 
+    }
+    notifyDeparture() {
+        
+    }
+}
