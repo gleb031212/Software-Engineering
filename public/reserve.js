5cbaee00819d42bd8580c9dd097383a1f8c41204
sampyle03 <118194414+sampyle03@users.noreply.github.com>
2024-05-14 17:21:23 +0100

    Basic Find Closest Car Park
    - carparks.html now looks nicer with more functionality
    - new reserve.html uses event listeners in reserve.js to find closest car park to approximate location (not using GPS)
    - CSS for cards moved from index.css to general.css for use outside of index.css

---
+++ public/reserve.js
@@ -0,0 +1,18 @@
+document.getElementById('location-ms').addEventListener('click', function() {
+    console.log("Closest Car Park: Blackdale Car Park");
+});
+document.getElementById('location-col').addEventListener('click', function() {
+    console.log("Closest Car Park: N&N Hospital");
+});
+document.getElementById('location-sp').addEventListener('click', function() {
+    console.log("Closest Car Park: UEA Main Car Park");
+});
+document.getElementById('location-su').addEventListener('click', function() {
+    console.log("Closest Car Park: SU Car Park");
+});
+document.getElementById('location-tw').addEventListener('click', function() {
+    console.log("Closest Car Park: Ziggurat Car Park");
+});
+document.getElementById('location-cd').addEventListener('click', function() {
+    console.log("Closest Car Park: Chancellors Drive Car Park");
+});
