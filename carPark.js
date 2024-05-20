41c7223432f997d79c9ef45eaf4b809aae8b07a3
sampyle03 <118194414+sampyle03@users.noreply.github.com>
2024-05-20 15:37:36 +0100

    New Checkout Page
    Other Minor Changes

---
+++ carPark.js
@@ -0,0 +1,55 @@
+class carPark{
+
+    constructor(id, name, totalSpaces, availableSpaces, vehicles){
+        this.carParkID = id;
+        this.name = name;
+        this.totalSpaces = totalSpaces;
+        this.availableSpaces = availableSpaces;
+        this.vehicles = vehicles;
+
+        this.spaceVisual = [];
+        for (let i = 0; i < this.totalSpaces; i++){
+            this.spaceVisual.push(null);
+        }
+    }
+
+    getCarParkID(){
+        return this.carParkID;
+    }
+
+    getTotalSpaces(){
+        return this.totalSpaces;
+    }
+
+    getAvailableSpaces(){
+        return this.availableSpaces;
+    }
+
+    getVehicles(){
+        return this.vehicles;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setTotalSpaces(totalSpaces){
+        this.totalSpaces = totalSpaces;
+    }
+
+    updateAvailableSpaces(){
+        this.availableSpaces = this.totalSpaces - this.vehicles.length;
+    }
+
+    assignSpace(vehicle){
+        // will find an available space in car park by finding null in spaceVisual, then assign the space to the vehicle by returning that space and replacing null with the vehicle object
+        for (let i = 0; i < this.spaceVisual.length; i++){
+            if (this.spaceVisual[i] === null){
+                this.spaceVisual[i] = vehicle;
+                return i;
+            }
+        }
+    }
+}
+
+export default carPark;
