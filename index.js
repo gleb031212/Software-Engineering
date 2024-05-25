815a2acf8cc1d686ecf812eaf3b1447e21e1478e
sampyle03 <118194414+sampyle03@users.noreply.github.com>
2024-05-25 22:40:34 +0100

    New Notify Page
    - Functionality in admin and system receiving data that user has arrived/department needs to be added

---
+++ index.js
@@ -0,0 +1,5 @@
+//event listener for #notify that listens for a click event and takes user to /notify securely
+document.getElementById('notify').addEventListener('click', function() {
+    window.location.href = '/notify';
+    }
+);
