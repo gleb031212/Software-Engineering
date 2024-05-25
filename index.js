e2f7e039bec5f650ba7407da8fd1802f0b4e60c4
sampyle03 <118194414+sampyle03@users.noreply.github.com>
2024-05-25 22:41:39 +0100

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-//event listener for #notify that listens for a click event and takes user to /notify securely
-document.getElementById('notify').addEventListener('click', function() {
-    window.location.href = '/notify';
-    }
-);
