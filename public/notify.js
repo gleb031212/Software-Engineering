858ed872d45cafd92cca19132f73b09e721d6510
sampyle03 <118194414+sampyle03@users.noreply.github.com>
2024-05-26 18:52:15 +0100

    Minor notify changes

--- public/notify.js
+++ public/notify.js
@@ -1,7 +1,21 @@
-console.log('notify.js loaded');
+function showPosition(position) {
+  let finalPosition = [position.coords.latitude, position.coords.longitude];
+  console.log('position:', finalPosition);
+}
+
+//https://www.w3schools.com/html/html5_geolocation.asp
 
 document.getElementById('notify-arrival').addEventListener('click', function() {
   // tell admin that user has arrived, then turn page to homepage
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+  
+
+
+
   window.location.href = '/';
 });
 
