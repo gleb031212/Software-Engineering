b1e154624d051cae2668147c36f37f30c188d7a1
Benjamaxo <76113399+Benjamaxo@users.noreply.github.com>
2024-05-27 21:06:36 +0100

    Register and Login with SQL!

--- public/login.js
+++ public/login.js
@@ -5,27 +5,31 @@ module.exports = {
   };
 const fs = require('fs');
 
+var mysql = require('mysql2');
+var connection = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "softeng",
+database: "parking_db"
+});
+connection.connect(function(err) {
+if (err) throw err;
+console.log("Connected Successfully! - login.js");
+});
+
 function login(username, password) {
-    console.log('Invalid username or password.')
-    fs.readFile('accounts.csv', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
+    var testquery1 = "SELECT * FROM users WHERE UserName LIKE '"+username+"' AND Password LIKE '"+password+"'";
+    connection.query(testquery1, function (err, returnlog){            
+        if (err){
+            console.log("Invalid username or password - SQL");
+        } 
+
+        else if (returnlog.length > 0) {
+            console.log(returnlog);
+            console.log('Login successful! - SQL');
         }
-        
-        const rows = data.split('\n');
-        
-        for (let i = 0; i < rows.length; i++) {
-            const columns = rows[i].split(',');
-            const storedUsername = columns[0];
-            const storedPassword = columns[1];
-            
-            if (storedUsername === username && storedPassword === password) {
-                console.log('Login successful!');
-                return;
-            }
+        else {
+            console.log("Invalid username or password - SQL");
         }
-        
-       console.log('Invalid username or password.');
-   });
+    })
 }
